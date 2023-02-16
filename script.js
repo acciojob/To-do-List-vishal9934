95df3764fb63a20beb424f746771cd647a7c2bfd
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-16 13:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var btn=document.getElementById("addTodoBtn");
 var ol=document.getElementById("todoList");
 
 function addTodo(){
-  if(todoValue){
+  if(todoValue.value){
     var li=document.createElement("li");
     li.textContent=todoValue.value;
     ol.appendChild(li);
